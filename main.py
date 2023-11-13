a536b7038c608bffbc1e1d053b8611376af576b4
Ramin jalalpoor <ramin.jalalpoor@mail.ru>
2023-11-13 21:24:13 +0300

    LAB_3

--- main.py
+++ main.py
@@ -1,59 +1,55 @@
 import tkinter as tk
-from tkinter import messagebox
-from sqrEq import sqrEquation
+import string
+import random
 
 
-def close():
-    window.destroy()
+def generate_key(hex_input):
+  """Generates a random key from the entered hexadecimal number."""
+  dec_num = str(int(hex_input, 16)).zfill(5)
+  options = string.ascii_uppercase + string.digits
+  parts = [
+      f"{dec_num[i]}{''.join(random.choices(options, k=4))}" for i in range(3)
+  ]
+  key = "-".join(parts + [dec_num[-2:]])
+  return key
 
 
-def calc():
-    A = float(arg_A.get())
-    B = float(arg_B.get())
-    C = float(arg_C.get())
-    if A == 0.0:
-        tk.messagebox.showwarning('Error', 'Division by zero!')
-    else:
-        lbl_result.configure(text=sqrEquation(A, B, C))
+def main():
+  window = tk.Tk()
+  window.title("Software Key Generator")
+  window.geometry('920x609')
+  window.resizable(False, False)
+
+  bg = tk.PhotoImage(file='background.png')
+  my_label = tk.Label(window, image=bg)
+  my_label.place(x=0, y=0)
+
+  tk.Label(window,
+           text="Enter a five-digit hexadecimal number:",
+           font=("Arial", 15)).grid(row=0, column=2, columnspan=2)
+  tk.Label(window, text="Generated key:",
+           font=("Arial", 12)).grid(row=12, column=2, columnspan=2)
+
+  entry = tk.Entry(window, font=("Arial", 12))
+  entry.insert(0, '00000')
+  entry.grid(row=1, column=3, columnspan=2)
+
+  generated_key = tk.Label(window,
+                           text="XXXXX-XXXXX-XXXXX-XX",
+                           font=("Arial", 12))
+  generated_key.grid(row=12, column=4, columnspan=2)
+
+  def press():
+    generated_key.config(text=generate_key(entry.get()))
+
+  start_button = tk.Button(window,
+                           text="Start",
+                           font=("Arial", 15),
+                           command=press)
+  start_button.grid(row=5, rowspan=2, column=3, columnspan=2)
+
+  window.mainloop()
 
 
-window = tk.Tk()
-window.geometry('576x360')
-bg_img = tk.PhotoImage(file='bg_pic.png')
-
-lbl_bg = tk.Label(window, image=bg_img)
-lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
-
-frame = tk.Frame(window)
-frame.place(relx=0.5, rely=0.5, anchor='center')
-
-lbl_A = tk.Label(frame, text='A', font=('Arial', 30), bg='blue', fg='white')
-lbl_A.grid(column=0, row=0, padx=10, pady=15)
-arg_A = tk.Entry(frame, width=10)
-arg_A.insert(0, '1')
-arg_A.grid(column=0, row=1, padx=10, pady=15)
-
-lbl_B = tk.Label(frame, text='B', font=('Arial', 30))
-lbl_B.grid(column=1, row=0, padx=10, pady=15)
-arg_B = tk.Entry(frame, width=10)
-arg_B.insert(0, '0')
-arg_B.grid(column=1, row=1, padx=10, pady=15)
-
-lbl_C = tk.Label(frame, text='C', font=('Arial', 30))
-lbl_C.grid(column=2, row=0, padx=10, pady=15)
-arg_C = tk.Entry(frame, width=10)
-arg_C.insert(0, '0')
-arg_C.grid(column=2, row=1, padx=10, pady=15)
-
-lbl_roots = tk.Label(frame, text='Result:')
-lbl_roots.grid(column=1, row=2)
-lbl_result = tk.Label(frame, text='None yet.', font=('Arial', 10))
-lbl_result.grid(column=2, row=2)
-
-btn_calc = tk.Button(frame, text='Calculate', command=calc)
-btn_calc.grid(column=0, row=3, padx=10, pady=15)
-btn_exit = tk.Button(frame, text='Cancel', command=close)
-btn_exit.grid(column=2, row=3, padx=10, pady=15)
-
-
-window.mainloop()
+if __name__ == "__main__":
+  main()
